591d0924493c7186bfd81e9ec22ba849523e7487
Anna Mrowicka <anna.mrowicka@atena.pl>
2021-04-17 13:15:38 +0200

    zadanie rekrutacyjne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,40 @@ import { AppComponent } from './app.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import { LogInComponent } from './main-content/log-in/log-in.component';
+import { FoodStoreComponent } from './main-content/book-store/food-store.component';
+import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MenuComponent } from './main-content/book-store/menu/menu.component';
+import { SelectedComponent } from './main-content/book-store/user-book-shelf/selected.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    FoodStoreComponent,
+    MenuComponent,
+    SelectedComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
